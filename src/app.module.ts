4e0b7d087d6c8c7392490e9554636571bd058d1d
Gabriel Fernández González <gfernandez573r@fpdrioja.com>
2024-03-28 13:11:34 +0100

    Eliminar archivos no usados y añadir estilos a la interfaz de VsCode

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import dbConfig from './config/db.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
